a3826be1b6d905b4458891219a31b4d58c207b70
cawfree <hello@cawfree.com>
2022-09-25 16:30:07 +0100

    chore: load ethers abi

---
+++ __tests__/index.test.ts
@@ -0,0 +1,25 @@
+import 'jest';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {ethers} from 'ethers';
+
+const OPENSEA_SEAPORT_V_1_1_FULFILL_BASIC_ORDER = /* ethers */
+  'function fulfillBasicOrder(tuple(address,uint256,uint256,address,address,address,uint256,uint256,uint8,uint256,uint256,bytes32,uint256,bytes32,bytes32,uint256,tuple(uint256,address)[],bytes)) payable returns (bool)';
+
+function loadFixture<T = unknown>({path: p}: {
+  readonly path: string;
+}) {
+  return JSON.parse(fs.readFileSync(path.resolve('__fixtures__', `${p}.json`), 'utf-8')) as T;
+}
+
+const loadSeaportv1_1 = () => new ethers.utils.Interface(
+  loadFixture({path: 'Seaport_v1_1'}),
+);
+
+describe('countersoiree', () => {
+  it('seaport_v11::abi', () => {
+    expect(loadSeaportv1_1()).toBeTruthy();
+  });
+});
